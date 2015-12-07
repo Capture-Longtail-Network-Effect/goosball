27dd80ec4942b697412b970c2e22499876cb67a9
dude-pwa <dudepwa@gmail.com>
2015-12-07 14:58:46 +0700

    lulus uji uniquenes email dan github :kissing_heart:

--- app/models/member.rb
+++ app/models/member.rb
@@ -1,4 +1,5 @@
 class Member < ActiveRecord::Base
 	validates :name, :role, :email, :github, presence: true
 	validates :email, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+	validates :email, :github, uniqueness: true
 end
