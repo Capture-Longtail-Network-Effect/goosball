30629b534caac0c1e74f1fc0e5f1965777f102eb
dude-pwa <dudepwa@gmail.com>
2015-12-08 17:42:54 +0700

    create simple pair form dan controller :sparkling_heart:

--- app/controllers/pairings_controller.rb
+++ app/controllers/pairings_controller.rb
@@ -8,13 +8,23 @@ class PairingsController < ApplicationController
 	end
 
 	def create
-		@pairing = Pairing.new(pairing_params)
+		@pairing = Pairing.new
+		@pairing.date = Date.new pairing_params["date(1i)"].to_i, 
+														 pairing_params["date(2i)"].to_i, 
+														 pairing_params["date(3i)"].to_i
+		@pairing.members_ids = [pairing_params[:member_1], pairing_params[:member_2]]
 		if @pairing.save
 			flash[:notice] = "Pairins created"
-			redirect_to pairins_path
+			redirect_to pairings_path
 		else
 			render 'new'
 		end
 	end
+
+	private
+	def pairing_params
+		params.require(:pairing).permit(:date, :member_1, :member_2)
+	end
+
 
 end
--- app/models/pairing.rb
+++ app/models/pairing.rb
@@ -1,2 +1,3 @@
 class Pairing < ActiveRecord::Base
+	attr_accessor :member_1, :member_2
 end
