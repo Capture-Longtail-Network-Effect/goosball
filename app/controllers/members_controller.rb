6894ef62329926b7a86c159561925ba17c78b0ab
Mufti <dev-null@prehistoric.me>
2015-12-11 06:50:13 +0000

    new member form, page, & controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,28 @@
 class MembersController < ApplicationController
     def index
+      @members = Member.all
     end
+
+    def new
+      @member = Member.new
+    end
+
+    def create
+      @member = Member.new(members_params)
+      if @member.save
+        flash[:notice] = 'New member saved'
+        redirect_to url_for action: 'index'
+      else
+        render 'new'
+      end
+    end
+
+    def edit
+      @member = Member.find(params[:id])
+    end
+
+    private 
+    def members_params
+      params.require(:member).permit(:name, :role, :email, :github)
+    end  
 end
